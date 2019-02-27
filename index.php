a52def1835f9ce16c95d5abd33706375cfddb0f9
duythuong <38066020+duythuong@users.noreply.github.com>
2019-02-27 19:30:17 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="contaier">
+        <div class="from">
+            <div class="logo">
+                <img src="avatar.jpg" alt="codergang">
+            </div>
+            <div class="box_1">
+                <h2>JK GROUP</h2>
+                <h2>VIETNAM BOT.</h2>
+            </div>
+            <div class="box_2">
+                <h4>A BOT THAT MAKES YOU A BETTER SOFTWARE ENGINEER.</h4>
+                
+            </div>  
+            <div class="btn">
+                    <i class="fab fa-facebook-messenger"></i>
+                    <span>Messenger</span>
+            </div>
+            <div class="link_chare">
+                <span><a href="#"><i class="fab fa-facebook-f"></i></a></span>
+                <span><a href="#"><i class="fab fa-instagram"></i></a></i></span>
+                <span><a href="#"><i class="fab fa-youtube"></i></a></i></span>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript" src="serve.js"></script>
+</body>
+</html>
